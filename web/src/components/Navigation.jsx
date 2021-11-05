0a199f4607f00a315abfa9d4664f04d66730b2a7
anvane-marcel <amarcelanvane@gmail.com>
2021-11-06 00:16:10 +0100

    fire host

---
+++ web/src/components/Navigation.jsx
@@ -0,0 +1,42 @@
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from '../pages/About';
+import Home from '../pages/Home';
+
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <Router>
+        <div>
+            <Box sx={{flexGrow: 1}}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{mr: 2}}>
+                            <MenuIcon />
+                        </IconButton>
+
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            VTC
+                        </Typography>
+
+                        <Button>
+                            <Link to="/" underline="none" className="lien_menu">Accueil</Link>
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+
+            <Routes>
+                <Route path="/about" element={<About />} />
+                <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        </Router>
+    );
+};
+
+export default Navigation;
---
+++ web/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+import React from 'react';
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1> 
+
+            <Button variant="contained">Hello</Button>
+        </div>
+    );
+};
+
+export default Home;
